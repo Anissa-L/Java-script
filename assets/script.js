f5285a1822390dc4b6c248858c35ec6b52f54f3c
Lamtalsi Anissa <lamtalsi.03@gmail.com>
2023-06-08 15:56:25 +0200

    arrow left

--- assets/script.js
+++ assets/script.js
@@ -20,22 +20,47 @@ const slides = [
 
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
-let dot = document.querySelector(".dot");
-let dots = document.querySelector(".dots");
-let dotSelected = document.querySelector(".dot_selected");
+const bannerImage = document.querySelector(".banner-img");
+const tagLine = document.querySelector("#banner p");
+const dots = document.querySelector(".dots");
+
+const lastSlide = slides.length - 1;
+
+// Create as many dots as slides in  the banner
+for (let i = 0; i < slides.length; i++) {
+  const dotDiv = document.createElement("div");
+  dotDiv.className = "dot";
+  dots.appendChild(dotDiv);
+}
+
+const allDots = document.querySelectorAll(".dot");
+allDots[0].classList.add("dot_selected");
+
+// Arrow left
 
 arrowLeft.addEventListener("click", () => {
+  console.log(index);
+  allDots[index].classList.remove("dot_selected");
+
+  if (index === 0) {
+    index = lastSlide; //if we are on the first slide make the next left click goes to the last slide
+  } else {
+    index--;
+  }
+
+  bannerImage.src = "./assets/images/slideshow/" + slides[index].image;
+  tagLine.innerHTML = slides[index].tagLine;
+  allDots[index].classList.add("dot_selected");
+
+  console.log(index);
+
+  allDots[index].classList.add("dot_selected");
+
   console.log("arrow Left !");
 });
 
+// Arrrow right
+
 arrowRight.addEventListener("click", () => {
   console.log("arrow right !");
-});
-
-dot.addEventListener("click", () => {
-  console.log("dot");
-});
-
-dot.addEventListener("click", () => {
-  dot.classList.add("dotSelected");
 });
